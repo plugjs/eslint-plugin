081fa10b5ad90840e9395be3c5a33aab9bb05a08
Pier Fumagalli <pier@usrz.com>
2022-11-04 10:37:48 +0100

    I will NEVER get globs right!

--- configs/typescript.cjs
+++ configs/typescript.cjs
@@ -121,7 +121,7 @@ module.exports = {
     'import/no-cycle': 'error',
     'import/no-duplicates': 'error',
     'import/no-extraneous-dependencies': [ 'error', {
-      'devDependencies': [ './test', './build.ts' ],
+      'devDependencies': [ 'test/**', 'build.ts' ],
       'peerDependencies': true,
       'optionalDependencies': true,
       'bundledDependencies': false,
