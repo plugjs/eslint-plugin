baa00096136436afc51607be1e52df6daedd3556
Pier Fumagalli <pier@usrz.com>
2024-11-19 16:33:51 +0100

    Create a default project to test TS rules in `check.mjs`

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,15 +2,11 @@ import configurations from './index.mjs'
 
 export default [
   ...configurations,
-
-  // ===== IGNORED FILES =======================================================
-  // REMEMBER! Ignores *must* be in its own configuration, they can not coexist
-  // with "rules", "languageOptions", "files", ... or anything else, otherwise
-  // ESLint will blaantly ignore the ignore files!
   {
-    ignores: [
-      'bundles/',
-      'node_modules/',
-    ],
+    languageOptions: {
+      parserOptions: {
+        projectService: true,
+      },
+    },
   },
 ]
