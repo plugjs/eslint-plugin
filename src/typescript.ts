3716d15c8ab3e6f412cec7c9bf347b762a887c03
Pier Fumagalli <pier@usrz.com>
2025-09-05 17:22:31 +0200

    Move to TypeScript and other enhancements: * Types * Config renames * Split "typescript-eslint/base" into a "plugin" definition   and all other configurations. We want the "plugin" unscoped   and the configurations scoped to .ts files.

--- src/typescript.ts
+++ src/typescript.ts
@@ -1,8 +1,52 @@
 import tseslint from 'typescript-eslint'
 
+import type { TSESLint } from '@typescript-eslint/utils'
+import type { ESLintConfig } from './index'
+
+/* ========================================================================== *
+ * INTERNALS                                                                  *
+ * ========================================================================== */
+
+/**
+ * Split the "base" config from "typescript-eslint" into its plugin definition
+ * and the rest of the config (namely, the language options). We'll want to
+ * apply the plugin **globally** so that all configs can use it, but the
+ * language options only to TypeScript files.
+ */
+const { plugins, ...opts } = tseslint.configs.base as TSESLint.FlatConfig.Config
+
+/** Map the "typescript-eslint" recommended configs */
+const configs = tseslint.configs.recommended.map((config: TSESLint.FlatConfig.Config) => {
+  // Allow the "@typescript-eslint" plugin to be visible everywhere...
+  if (config.name === tseslint.configs.base.name) return null
+  config.files = [ '**/*.ts', '**/*.cts', '**/*.mts' ]
+  return config
+}).filter((config): config is ESLintConfig => !! config)
+
+/* ========================================================================== *
+ * CONFIGS                                                                    *
+ * ========================================================================== */
+
+/** Define the "@typescript-eslint" plugin. */
+export const plugin: ESLintConfig<'plugjs/typescript/plugin'> = {
+  name: 'plugjs/typescript/plugin',
+  plugins,
+}
+
+/* ========================================================================== */
+
+/** Configuration for "@typescript-eslint" scoped to TypeScript sources. */
+export const options: ESLintConfig<'plugjs/typescript/options'> = {
+  ...opts,
+  files: [ '**/*.ts', '**/*.cts', '**/*.mts' ],
+  name: 'plugjs/typescript/options',
+}
+
+/* ========================================================================== */
+
 /** Our own rules overriding `typescript-eslint/recommended`. */
-export const typescript = {
-  name: 'plugjs-typescript',
+export const overrides: ESLintConfig<'plugjs/typescript/overrides'> = {
+  name: 'plugjs/typescript/overrides',
 
   files: [ '**/*.ts', '**/*.cts', '**/*.mts' ],
 
@@ -33,19 +77,10 @@ export const typescript = {
  *
  * This module includes these configurations:
  *
- * * `typescript-eslint/recommended`: imports all the configurations from
- *   TypeScript ESlint recommended, but restrict them to operate only on
- *   `.ts`, `.cts`, and `.mts` files. This *should* include:
- *   * `typescript-eslint/base`: basic parser configuration.
- *   * `typescript-eslint/eslint-recommended`: disable ESLint rules conflicting
- *     with TypeScript.
- *   * `typescript-eslint/recommended`: recommended config for TypeScript
- * * `plugjs-typescript`: our rules overriding `typescript-eslint/recommended`.
+ * * `plugjs/typescript/plugin`: defines the "@typescript-eslint" plugin.
+ * * `plugjs/typescript/options`: configuration for "@typescript-eslint".
+ * * other rules from `typescript-eslint/recommended`: all other recommended
+ *   Typescript rules, restricted to `.ts`, `.cts`, and `.mts` files.
+ * * `plugjs/typescript/overrides`: our rules overriding recommended.
  */
-export default [
-  ...tseslint.configs.recommended.map((config) => {
-    config.files = [ '**/*.ts', '**/*.cts', '**/*.mts' ]
-    return config
-  }),
-  typescript,
-]
+export default [ plugin, options, ...configs, overrides ] as const
