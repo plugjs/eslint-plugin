174bccb57d9f522cd04f7804d6486a79dc6d6f6f
Pier Fumagalli <pier@usrz.com>
2025-09-05 17:22:53 +0200

    Rework export, comments, types, ...

--- src/index.ts
+++ src/index.ts
@@ -1,44 +1,72 @@
 import js from '@eslint/js'
 
-import basic from './configs/basic.mjs'
-import javascript from './configs/javascript.mjs'
-import typescript from './configs/typescript.mjs'
+import basicConfigs, * as basic from './basic.js'
+import javascriptConfigs, * as javascript from './javascript.js'
+import typescriptConfigs, * as typescript from './typescript.js'
 
-export * from './configs/basic.mjs'
-export * from './configs/javascript.mjs'
-export * from './configs/typescript.mjs'
+import type { TSESLint } from '@typescript-eslint/utils'
+
+/* ========================================================================== *
+ * TYPES                                                                      *
+ * ========================================================================== */
+
+/** A _named_ ESLint configuration. */
+export type ESLintConfig<N extends string = string> = {
+  readonly name: N
+} & TSESLint.FlatConfig.Config
+
+/* ========================================================================== *
+ * INTERNAL                                                                   *
+ * ========================================================================== */
+
+/* Give the "eslint/js/recommended" a name so that it shows up in the GUI */
+const initial: ESLintConfig<'eslint/js/recommended'> = { // give a name to this config before we go completely nuts!
+  name: 'eslint/js/recommended',
+  ...js.configs.recommended,
+}
+
+/* ========================================================================== *
+* EXPORTS                                                                    *
+ * ========================================================================== */
+
+/** All ESLint configurations. */
+export const configs = {
+  'basic': basicConfigs,
+  'basic/base': basic.base,
+  'basic/stylistic': basic.stylistic,
+  'basic/unicorn': basic.unicorn,
+  'basic/importx': basic.importx,
+
+  'javascript': javascriptConfigs,
+  'javascript/shared': javascript.shared,
+  'javascript/commonjs': javascript.commonjs,
+  'javascript/modules': javascript.modules,
+
+  'typescript': typescriptConfigs,
+  'typescript/plugin': typescript.plugin,
+  'typescript/options': typescript.options,
+  'typescript/overrides': typescript.overrides,
+} as const
 
 /**
- * Base `ESLint` configuration for PlugJS.
+ * The default ESLint configuration for PlugJS.
  *
- * This includes a number of configurations:
- *
- * * `eslint-recommended`: recommended JavaScript config from ESLint.
- *
- * * `plugjs-base`: basic configuration of ESLint rules.
- * * `plugjs-stylistic`: style shared between JavaScript and TypeScript.
- * * `plugjs-unicorn`: extra niceties from the ESLint Unicorn plugin.
- * * `plugjs-importx`: defines the style of our imports.
- *
- * * `plugjs-javascript`: basic extra rules for JavaScript sources.
- * * `plugjs-javascript-cjs`: marks `*.cjs` files as `commonjs`.
- * * `plugjs-javascript-mjs`: marks `*.mjs` files as `module`.
- *
- * * `typescript-eslint/recommended`: imports all the configurations from
- *   TypeScript ESlint recommended, but restrict them to operate only on
- *   `.ts`, `.cts`, and `.mts` files. This *should* include:
- *   * `typescript-eslint/base`: basic parser configuration.
- *   * `typescript-eslint/eslint-recommended`: disable ESLint rules conflicting
- *     with TypeScript.
- *   * `typescript-eslint/recommended`: recommended config for TypeScript
- * * `plugjs-typescript`: our rules overriding `typescript-eslint/recommended`.
- */
+ * This includes all the configurations from:
+ * * `eslint/js/recommended`: the base ESLint recommended rules for JavaScript.
+ * * `plugjs/basic`: basic rules shared between JavaScript and TypeScript.
+ *   * `plugjs/basic/base`: basic configuration of ESLint rules.
+ *   * `plugjs/basic/stylistic`: style shared between JavaScript and TypeScript.
+ *   * `plugjs/basic/unicorn`: extra niceties from the ESLint Unicorn plugin.
+ *   * `plugjs/basic/importx`: defines the style of our imports.
+ * * `plugjs/javascript`: rules specific to JavaScript code bases.
+ *   * `plugjs/javascript/shared`: shared configuration for JavaScript files.
+ *   * `plugjs/javascript/commonjs`: marks `*.cjs` files as `commonjs`.
+ *   * `plugjs/javascript/modules`: marks `*.mjs` files as `module`.
+ * * `plugjs/typescript`: rules specific to TypeScript code bases.
+ *  */
 export default [
-  { // give a name to this config before we go completely nuts!
-    name: 'eslint/js/recommended',
-    ...js.configs.recommended,
-  },
-  ...basic,
-  ...javascript,
-  ...typescript,
-]
+  initial,
+  ...basicConfigs,
+  ...javascriptConfigs,
+  ...typescriptConfigs,
+] as const
